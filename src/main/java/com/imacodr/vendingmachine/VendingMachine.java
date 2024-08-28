eb4c01191eae6f7437a6c1b688d799135edb21f3
Sam Perillo <samuel.perillo@gmail.com>
2024-08-27 22:33:01 -0600

    first commit

---
+++ src/main/java/com/imacodr/vendingmachine/VendingMachine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.imacodr.vendingmachine;
+
+/**
+ *
+ * @author samperillo
+ */
+public class VendingMachine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
